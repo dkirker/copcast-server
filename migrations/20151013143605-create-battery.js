4ce1dd63b5c17602e768047ca656fcdad0be163b
brunosiqueira <bsiqueira@gmail.com>
2015-10-15 18:03:05 +0100

    correcting database type #133

--- migrations/20151013143605-create-battery.js
+++ migrations/20151013143605-create-battery.js
@@ -20,7 +20,7 @@ module.exports = {
           type: Sequelize.INTEGER
         },
         plugged: {
-          type: Sequelize.BOOLEAN
+          type: Sequelize.INTEGER
         },
         temperature: {
           type: Sequelize.INTEGER
