0a4bae4b828da9c3aa048e69bff7223b4ea44b0b
Leandro Martelli <martelli@epix.com.br>
2015-10-20 09:44:06 -0200

    Migration to create incidents table

---
+++ migrations/20151019174455-create-incidents.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize, done) {
+    queryInterface.createTable(
+      'incidents',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        date: {
+          type: Sequelize.DATE
+        },
+        userId: {
+          type: Sequelize.BIGINT,
+          references: {
+            model: 'users',
+            key: 'id'
+          }
+        },
+
+      }
+    ).then(function() {
+        console.log(queryInterface.models);
+        done();
+      });
+  },
+
+  down: function (queryInterface, Sequelize, done) {
+    queryInterface.dropTable('incidents').then(function() {
+      done();
+    })  }
+};
