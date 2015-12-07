ba86f41575b707fc537fe54f79d28fd4813d4d67
Leandro Martelli <martelli@epix.com.br>
2015-12-07 11:22:58 -0200

    config parameter: rabbitmq.prefetchCount

--- consumer.js
+++ consumer.js
@@ -6,29 +6,21 @@ var db = require('./lib/db')
   , config = require('./lib/config')
   , rabbitmq = require('./lib/rabbitmq')
   , storage = require('./lib/videos/storage')
-
   , crypto = require('./lib/crypto');
 
 
-
 var option = {force: false};
-//if (process.env.NODE_ENV == "test"){
-//  option["force"] = true;
-//}
 
 db.sequelize.sync(option).then(function () {
-  //if (err) {
-  //  throw err;
-  //} else {
   crypto.crypto_init(function () {
     rabbitmq.init(function () {
       rabbitmq.connection().queue('copcast-q', function(q) {
         console.log('Queue ' + q.name + ' is open');
-        q.bind(rabbitmq.exchange(), 'copcast-k', function(o) {
-          console.log('ready: '+config.parallel_videos);
+        q.bind(rabbitmq.exchange(), 'copcast-k', function() {
+          console.log('Consumer ready. Maximum parallel executions: '+config.rabbitmq.prefetchCount);
           q.subscribe({ack: true, prefetchCount: config.parallel_videos}, function (message, headers, deliveryInfo, ack) {
             try{
-              storage.ingestVideo(message.path, message.user.id, message.date, function(code) {
+              storage.ingestVideo(message.path, message.user.id, message.date, function() {
                 console.log(message.path);
                 ack.acknowledge();
               });
@@ -40,4 +32,8 @@ db.sequelize.sync(option).then(function () {
       });
     });
   });
+}).catch(function(err) {
+  console.log('Failed to access the database.');
+  console.log(err);
+  process.exit(-1);
 });
