cc33c7c52e66274ee9243d9583b2ab38ebb96e14
brunosiqueira <bsiqueira@gmail.com>
2016-02-15 16:15:15 +0000

    igarape/copcast-admin#209 new reset password schema

--- migrations/20160211173546-add-remember-token.js
+++ migrations/20160211173546-add-remember-token.js
@@ -7,20 +7,13 @@ module.exports = {
       'users',
       'rememberToken',
       {
-        type: Sequelize.STRING(20)
+        type: Sequelize.STRING
       }
     );
 
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-     Add reverting commands here.
-     Return a promise to correctly handle asynchronicity.
-
-     Example:
-     return queryInterface.dropTable('users');
-     */
     queryInterface.removeColumn(
       'users',
       'rememberToken'
