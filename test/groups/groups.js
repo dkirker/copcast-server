139be3560410f7dfe874242b402db46e71dd189c
brunosiqueira <bsiqueira@gmail.com>
2016-05-12 17:57:05 -0300

    groups tests corrected

--- test/groups/groups.js
+++ test/groups/groups.js
@@ -1,13 +1,17 @@
 var request = require('supertest'),
   should = require('should'),
+  app = require('express')(),
   proxyquire = require('proxyquire'),
   auth = require('./../mocks/auth'),
   db = require('./../../lib/db'),
-  groups = proxyquire('./../../lib/groups', {'./../auth': auth, './../db': db}),
-  http = require('http'),
-  server = http.createServer(groups),
-  api = request(server),
-  factory = require('./../setup');
+  groups = proxyquire('./../../lib/groups', {'./../auth': auth, './../db': db})
+  , bodyParser = require('body-parser'), factory = require('./../setup');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(groups);
 
 describe('Groups Tests', function () {
   var groupAdmin = null;
@@ -26,11 +30,11 @@ describe('Groups Tests', function () {
     });
   });
 
-  describe('List groups', function(){
-    it('shows groups',function(done){
-      api.get('/groups')
+  describe('List groups', function () {
+    it('shows groups', function (done) {
+      request(app).get('/groups')
         .expect(200)
-        .end(function(err, res) {
+        .end(function (err, res) {
           should(err).be.null();
           should(res.body.length).equal(1);
           done();
@@ -43,69 +47,66 @@ describe('Groups Tests', function () {
       var groupData = {
         name: 'Test group'
       };
-      api.post('/groups')
+      request(app).post('/groups')
         .send(groupData)
+        .type('json')
         .expect(200)
         .end(function (err, res) {
-         done()
+          done()
         });
     });
-    it('fails with required fields blank',function(done){
-      api.post('/groups')
-    .type('json')
+    it('fails with required fields blank', function (done) {
+      request(app).post('/groups')
+        .type('form')
         .send({})
         .expect(403)
-        .end(function(err, res) {
-            done();
+        .end(function (err, res) {
+          done();
         });
     });
   });
 
-  describe('Update groups', function(){
-    it('update successfully',function(done){
-  
+  describe('Update groups', function () {
+    it('update successfully', function (done) {
+
       var name = 'NEW NAME';
-        api.post('/groups/'+groupAdmin.id)
-          .send({name: name})
-  .type('json')
+      request(app).post('/groups/' + groupAdmin.id)
+        .send({name: name})
+        .type('form')
         .expect(200)
-        .end(function(err, res) {
-          db.user.findById(group.id).then(function(g){
+        .end(function (err, res) {
+          db.group.findById(groupAdmin.id).then(function (g) {
             should(g.name).equal(name);
             done();
           })
         });
     });
-    it('fails with required fields blank',function(done){
-  
-      var name = 'NEW NAME';
-      api.post('/groups/'+groupAdmin.id)
+    it('fails with required fields blank', function (done) {
+
+      request(app).post('/groups/' + groupAdmin.id)
         .send({name: null})
-  .type('json')
+        .type('form')
         .expect(403)
 
-        .end(function(err, res) {
-          db.user.findById(group.id).then(function(g){
-            should(g.name).equal(name);
+        .end(function (err, res) {
             done();
-          })
         });
     });
   });
-  
-  describe('Delete group', function(){
+
+  describe('Delete group', function () {
     var groupNoUser = null;
-    beforeEach(function(done){
-      factory.create('groupNoUser', function(err, g){
+    beforeEach(function (done) {
+      factory.create('groupNoUser', function (err, g) {
         groupNoUser = g;
         done();
       });
     });
-    it('delete successfully',function(done){
-      api.delete('/groups/'+groupNoUser.id)
+    it('delete successfully', function (done) {
+      request(groups).delete('/groups/' + groupNoUser.id)
         .expect(200)
-        .end(function(err, res) {
-          db.user.findById(groupNoUser.id).then(function(g){
+        .end(function (err, res) {
+          db.group.findById(groupNoUser.id).then(function (g) {
             should(g).be.null();
             done();
           })
