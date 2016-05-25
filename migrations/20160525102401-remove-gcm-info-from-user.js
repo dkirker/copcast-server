dc082f9732745bd46a01de597847f5c11a3e24a5
brunosiqueira <bsiqueira@gmail.com>
2016-05-25 18:00:37 -0300

    correcting GCM migration script

--- migrations/20160525102401-remove-gcm-info-from-user.js
+++ migrations/20160525102401-remove-gcm-info-from-user.js
@@ -2,10 +2,22 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.removeColumn('users', 'gcmRegistration');
+    queryInterface.sequelize.query('DROP VIEW users_active').then(function() {
+      queryInterface.removeColumn('users', 'gcmRegistration').then(function(){
+        queryInterface.sequelize.query('CREATE VIEW users_active AS SELECT * FROM users WHERE "deletedAt" IS NULL');
+      });
+    });
   },
 
   down: function (queryInterface, Sequelize) {
-    return queryInterface.createColumn('users', 'gcmRegistration', Sequelize.STRING(1024));
+    return queryInterface.addColumn('users', 'gcmRegistration', Sequelize.STRING(1024)).then(function(){
+      return queryInterface.sequelize.query('ALTER VIEW users_active ADD "gcmRegistration" STRING(1024)');
+    });
+
+    queryInterface.sequelize.query('DROP VIEW users_active').then(function() {
+      queryInterface.addColumn('users', 'gcmRegistration', Sequelize.STRING(1024)).then(function(){
+        queryInterface.sequelize.query('CREATE VIEW users_active AS SELECT * FROM users WHERE "deletedAt" IS NULL');
+      });
+    });
   }
 };
