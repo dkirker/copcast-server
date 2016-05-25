d0f2adf690dfcd5575766d95b16ecd2fefa799ae
brunosiqueira <bsiqueira@gmail.com>
2016-05-25 18:21:46 -0300

    updating RECORDING_ONLINE and RECORDING_OFFLINE states to RECORDING

--- migrations/20160525102401-remove-gcm-info-from-user.js
+++ migrations/20160525102401-remove-gcm-info-from-user.js
@@ -1,22 +1,21 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
+  up: function (queryInterface, Sequelize, done) {
     queryInterface.sequelize.query('DROP VIEW users_active').then(function() {
       queryInterface.removeColumn('users', 'gcmRegistration').then(function(){
         queryInterface.sequelize.query('CREATE VIEW users_active AS SELECT * FROM users WHERE "deletedAt" IS NULL');
+        done();
       });
     });
   },
 
-  down: function (queryInterface, Sequelize) {
-    return queryInterface.addColumn('users', 'gcmRegistration', Sequelize.STRING(1024)).then(function(){
-      return queryInterface.sequelize.query('ALTER VIEW users_active ADD "gcmRegistration" STRING(1024)');
-    });
+  down: function (queryInterface, Sequelize, done) {
 
     queryInterface.sequelize.query('DROP VIEW users_active').then(function() {
       queryInterface.addColumn('users', 'gcmRegistration', Sequelize.STRING(1024)).then(function(){
         queryInterface.sequelize.query('CREATE VIEW users_active AS SELECT * FROM users WHERE "deletedAt" IS NULL');
+        done();
       });
     });
   }
