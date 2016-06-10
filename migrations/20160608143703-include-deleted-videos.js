d80ff7d5177da9277ad502d6f30cb3406f6ae989
brunosiqueira <bsiqueira@gmail.com>
2016-06-10 19:49:21 -0300

    igarape/copcast-android#37 creating initial background job and fields in the database

--- migrations/20160608143703-include-deleted-videos.js
+++ migrations/20160608143703-include-deleted-videos.js
@@ -1,23 +1,19 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+  up: function (queryInterface, Sequelize, done) {
+    queryInterface.addColumn('videos','isDeleted',  Sequelize.BOOLEAN, {default: false} ).then(function(){
+      queryInterface.addColumn('videos','hasIncident', Sequelize.BOOLEAN, {default: false} ).then(function(){
+        done()
+      });
+    });
   },
 
-  down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+  down: function (queryInterface, Sequelize, done) {
+    queryInterface.removeColumn('videos','isDeleted').then(function(){
+      queryInterface.removeColumn('videos','hasIncident').then(function() {
+        done();
+      });
+    });
   }
 };
