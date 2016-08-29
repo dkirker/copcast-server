34d43ffe042aa3655a563f8416410794ee612ae2
Leandro Martelli <martelli@epix.com.br>
2016-08-29 13:14:55 -0300

    Include new parameters (key length, encryption and hash algorithms) in cryptoGenerator.js

--- cryptoConfigGenerator.js
+++ cryptoConfigGenerator.js
@@ -1,11 +1,17 @@
+
+
+
 var crypt = require('./lib/crypto')
 
-crypt.challenge(function (key, salt, partitions, challenge) {
+crypt.challenge(function (key, salt, partitions, challenge, encalgo, kl, hashalgo) {
   console.log('"crypto": {\n' +
     '  "partitions": ' + partitions + ',\n' +
     '  "key": "' + key + '",\n' +
     '  "challenge": "' + challenge + '",\n' +
     '  "salt": "' + salt + '"\n' +
+    '  "encryption_algorithm": "' + encalgo + '"\n' +
+    '  "key_length": "' + kl + '"\n' +
+    '  "key_hash_algorithm": "' + hashalgo + '"\n' +
     '}\n'
   );
   process.exit();
