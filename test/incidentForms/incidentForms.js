8d1f67ec939133eb4e93259c6fe3cc463c6a3506
Kelly Davis <kldavis4@gmail.com>
2016-08-31 15:47:02 -0400

    Add validation of incidentForm and simple tests - Also commit of missing incidents tests

---
+++ test/incidentForms/incidentForms.js
@@ -0,0 +1,88 @@
+var request = require('supertest'),
+  should = require('should'),
+  app = require('express')(),
+  proxyquire = require('proxyquire'),
+  auth = require('./../mocks/auth'),
+  db = require('./../../lib/db'),
+  rest = proxyquire('./../../lib/incidentForms', {'./../auth': auth, './../db': db}),
+  bodyParser = require('body-parser'),
+  config = require('./../../lib/config'),
+  factory = require('./../setup');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(rest);
+
+auth.scope = 'client'; //set mock user scope
+
+describe('Bulk incidentForms upload', function() {
+  beforeEach(function (done) {
+    config.signatureVerification = false;
+    db.sequelize.sync({force: true}).then(function (err) {
+      factory.create('testUser', function (err, u) {
+        if (err) {
+          console.log(err);
+          done();
+        } else {
+          auth.user = u;  //set mock user
+          done();
+        }
+      });
+    });
+  });
+
+  describe('Bulk uploads',function(){
+    it('will upload one item', function (done) {
+      request(app).post('/incidentForms/testuser')
+        .send({"bulk": [{"date": "2016-08-19T23:11:15.123Z", "lat": 82, "lng": 42}]})
+        .expect(201)
+        .end(function (err, res) { should.not.exist(err); done(); });
+    });
+
+    it('will upload one fake array like', function (done) {
+      request(app).post('/incidentForms/testuser')
+        .send({"bulk": {"length" : 10}})
+        .expect(400)
+        .end(function (err, res) { should.not.exist(err); done(); });
+    });
+
+    it('will upload one malicious json', function (done) {
+      request(app).post('/incidentForms/testuser')
+        .send({"bulk": [{"lat": {"$iLike": "%"}, "lng": 42, "date": "2016-08-19T23:11:15.123Z"}]})
+        .expect(400)
+        .end(function (err, res) { should.not.exist(err); done(); });
+    });
+  });
+
+  describe('Individual uploads',function(){
+    it('sends valid incident request',function(done) {
+      request(app).post('/incidentForms')
+        .send({"date": "2016-08-19T23:11:15.123Z", "lat": 82, "lng": 42})
+        .expect(201)
+        .end(function (err, res) {
+          should.not.exist(err);
+          done();
+        });
+    });
+
+    it('will upload one malicious json in lat', function (done) {
+      request(app).post('/incidentForms')
+        .send({"lat": {"$iLike": "%"}, "lng": 42, "date": "2016-08-19T23:11:15.123Z"})
+        .expect(400)
+        .end(function (err, res) { should.not.exist(err); done(); });
+    });
+
+    it('will upload one malicious json in lng', function (done) {
+      request(app).post('/incidentForms')
+        .send({"lng": {"$iLike": "%"}, "lat": 42, "date": "2016-08-19T23:11:15.123Z"})
+        .expect(400)
+        .end(function (err, res) { should.not.exist(err); done(); });
+    });
+
+    it('will upload one malicious json in lng', function (done) {
+      request(app).post('/incidentForms')
+        .send({"date": {"$iLike": "%"}, "lat": 82, "lng": 42})
+        .expect(400)
+        .end(function (err, res) { should.not.exist(err); done(); });
+    });
+  });
+});
