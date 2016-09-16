3b78e5ce5eb14016668a3e6a2f0620c18513410a
Leandro Martelli <martelli@epix.com.br>
2016-09-16 02:18:58 -0300

    Fix missing commas

--- cryptoConfigGenerator.js
+++ cryptoConfigGenerator.js
@@ -8,11 +8,11 @@ crypt.challenge(function (key, salt, partitions, challenge, encalgo, kl, hashalg
     '  "partitions": ' + partitions + ',\n' +
     '  "key": "' + key + '",\n' +
     '  "challenge": "' + challenge + '",\n' +
-    '  "salt": "' + salt + '"\n' +
-    '  "encryption_algorithm": "' + encalgo + '"\n' +
-    '  "key_length": "' + kl + '"\n' +
+    '  "salt": "' + salt + '",\n' +
+    '  "encryption_algorithm": "' + encalgo + '",\n' +
+    '  "key_length": "' + kl + '",\n' +
     '  "key_hash_algorithm": "' + hashalgo + '"\n' +
-    '}\n'
+    '},\n'
   );
   process.exit();
 });
